1332308331d9fc700508a593bba9b0d86f5dd6d2
GCodeOn <gerardosoto93@gmail.com>
2018-07-21 10:09:06 -0700

    About page hero updates

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,39 +2,35 @@
 
 @section ('content')
 <header>
-    @include('partials.hero', $hero)
+    @include('partials.hero-about', $hero)
 </header>
 
 <section class="grid-like">
     <div class="row">
         <div class="column small-12 medium-7 medium-push-5">
             <h3>
+                 Our Mission
+            </h3>
+            <p>
+                SoftStack Factory is a nonprofit focused on the technology job skills gap, applying an immersion bootcamp model with an on the job training spin, to train qualified young adults to meet the high demand jobs of the digital economy.
+            </p>
+        </div>
+        <div class="column small-12 medium-5 medium-pull-7 image-full">
+            <img src="/images/banner-img-volunteer.png" alt="" />
+        </div>
+    </div>
+    <div class="row margin-top-2">
+        <div class="column medium-7">
+        <h3>
                 More than a Coding Bootcamp!
             </h3>
             <p>
                 SoftStack Factory is a network of students, volunteers, sponsors and partners empowering the local San Diego community.
             </p>
-        </div>
-        <div class="column small-12 medium-5 medium-pull-7 image-full">
-            <img src="/images/retail-stores.png" alt="" />
-        </div>
-    </div>
-    <div class="row margin-top-2">
-        <div class="column medium-7">
-            <h3>
-                Currently CounterBalance is working to raise awareness about the dangers of flavored tobacco products.
-            </h3>
-            <p>
-                The tobacco industry uses candy flavors and colorful packaging to make e-cigarettes, little cigars, and chew seem more appealing. In fact, two-thirds of
-                tobacco-using youth said they use these products because they come in flavors they like.
-            </p>
-            <p>
-                Find out what you can do to take a stand against flavored tobacco.
-            </p>
             <a href="/get-involved" class="button">Get Involved</a>
         </div>
         <div class="column medium-5 hide-for-small-only">
-            <img src="/images/currently.png" alt=""/>
+            <img src="/images/banner-group.png" alt=""/>
         </div>
     </div>
 
---
+++ resources/views/partials/hero-about.blade.php
@@ -0,0 +1,51 @@
+<div class="hero-bg hero">
+    <div class="expanded row">
+        <div class="columns small-12 medium-7 large-5 left-container">
+            <div class="video-wrapper image-wrapper">
+                @if(Request::is('/'))
+                    <video class="full-media" autoplay playsinline muted loop>
+                        <source src="https://player.vimeo.com/external/269681229.sd.mp4?s=ccde78fc1159a26dce8c37210bac058715c6219d&profile_id=165" type="video/mp4">
+                    </video>
+
+                    <div class="video-overlay"></div>
+
+                    <div class="video-control-content">
+                        <div class="video-play" data-analytics-action="start" data-analytics-category="video">
+                            <a data-toggle="heroModal" data-analytics-click data-analytics-label="wntb-61-townhall">
+                                <i class="fa fa-play-circle" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                        <div class="title">Stand up for vermont’s youth</div>
+                    </div>
+                @endif
+            </div>
+        </div>
+        <div class="columns small-12 medium-5 large-7 right-container">
+            <div class="right-content">
+                @if($title)
+                    <h1>{{$title}}</h1>
+                @endif
+                <p> {{$content}}</p>
+            </div>
+        </div>
+
+
+        <div class="image-overlay"></div>
+    </div>
+
+
+</div>
+<div id="made-in-ny"></div>
+<div class="reveal full" id="heroModal" data-reveal data-reveal>
+    <div class="reveal-body">
+        <button class="close-button" data-close aria-label="Close reveal" type="button">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <div class='embed-container'>
+            <iframe class="heroModal-iframe vimeo-analytics-iframe" data-analytics-percents="10,20,30,40,50,60,70,80,90" src="https://player.vimeo.com/video/257015422" width="640"
+                    height="360"
+                    frameborder="0"
+                    webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
